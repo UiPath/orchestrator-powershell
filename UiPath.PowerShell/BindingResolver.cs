8e2f0a3deb4b9550bd673d94379326af5662c381
Remus Rusanu <remus.rusanu@uipath.com>
2018-05-18 17:30:24 +0300

    .Net 4.5.2, Newtonsoft 10 to match Robot, tollerant bindign resolver

--- UiPath.PowerShell/BindingResolver.cs
+++ UiPath.PowerShell/BindingResolver.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UiPath.PowerShell
 {
@@ -16,7 +12,7 @@ namespace UiPath.PowerShell
         {
             AppDomain.CurrentDomain.AssemblyResolve += delegate (object sender, ResolveEventArgs args)
             {
-                if (args.Name.Equals("Newtonsoft.Json, Version=6.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed", StringComparison.InvariantCultureIgnoreCase))
+                if (args.Name.StartsWith("Newtonsoft.Json", StringComparison.InvariantCultureIgnoreCase))
                 {
                     var path = Path.Combine(
                         Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
