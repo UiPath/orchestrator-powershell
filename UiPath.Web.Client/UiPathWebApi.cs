481f85a85f3504108fd3f13a6b6d503f7317a43f
Remus Rusanu <remus.rusanu@uipath.com>
2018-11-01 14:02:26 +0200

    Better MakeApi

---
+++ UiPath.Web.Client/UiPathWebApi.cs
@@ -0,0 +1,20 @@
+﻿namespace UiPath.Web.Client20181
+{
+    public partial class UiPathWebApi: Client.Generic.IUiPathWebApi
+    {
+    }
+}
+
+namespace UiPath.Web.Client20182
+{
+    public partial class UiPathWebApi : Client.Generic.IUiPathWebApi
+    {
+    }
+}
+
+namespace UiPath.Web.Client20183
+{
+    public partial class UiPathWebApi : Client.Generic.IUiPathWebApi
+    {
+    }
+}
