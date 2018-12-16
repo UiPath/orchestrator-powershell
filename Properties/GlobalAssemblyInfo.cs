8e07877e690c29aaa8d5fb2678401c1c7f438308
Remus Rusanu <remus.rusanu@uipath.com>
2018-12-16 22:21:03 +0200

    Use VSTS counter for revision

--- Properties/GlobalAssemblyInfo.cs
+++ Properties/GlobalAssemblyInfo.cs
@@ -10,5 +10,8 @@ using System.Reflection;
 
 
 // The version should be set to match the current released platform version
+// IMPORTANT: see .vsts-ci.yml $patch variable
+// Remeber to reset the counter on each version bump
+// Eg. if you change version here to 18.4.2.*, in .vsts-ci.yml the line should be patch: $[counter('patch_counter_18_4_2', 1)]
 //
 [assembly: AssemblyVersion("18.4.1.*")]
