373c9459d061c9149f3362d442e82b6027bf8ce0
Remus Rusanu <remus.rusanu@uipath.com>
2018-12-21 23:19:54 +0200

    Force default TLS protocol 1.1 and 1.2, don't accept Ssl3 and 1.0

---
+++ UiPath.PowerShell/Util/SecurityProtocolFix.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace UiPath.PowerShell.Util
+{
+    internal class SecurityProtocolFix
+    {
+        internal static bool Ignored = false;
+
+        static SecurityProtocolFix()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+        }
+    }
+}
