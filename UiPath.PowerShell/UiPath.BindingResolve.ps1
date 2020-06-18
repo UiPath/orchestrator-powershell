3de2d3c5219f0fd357323142527381d1a6ff854a
Remus Rusanu <remus.rusanu@uipath.com>
2020-06-18 14:04:27 +0300

    .Net 4.7

---
+++ UiPath.PowerShell/UiPath.BindingResolve.ps1
@@ -0,0 +1,22 @@
+﻿$OnAssemblyResolve = [System.ResolveEventHandler] {
+  param($sender, $e)
+
+
+  if ($e.Name.StartsWith("System.Net.Http,"))
+  {
+    [system.diagnostics.debug]::WriteLine("Binding redirect $($e.Name) resolved to $($script:httpNet.Location)")
+    return $script:httpNet
+  }
+
+  return $null
+}
+
+trap [System.Exception] { [system.diagnostics.debug]::WriteLine("Exception: $_") }
+
+if ($env:UIPATH_POWERSHELL_SKIP_BINDING_REDIRECT -ne $true)
+{
+  Write-Verbose "Installing assembly resolve binding redirect callback"
+  # See https://github.com/dotnet/runtime/issues/20777#issuecomment-338418610 
+  $script:httpNet = [reflection.assembly]::Load("System.Net.Http, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
+  [System.AppDomain]::CurrentDomain.add_AssemblyResolve($OnAssemblyResolve)
+}
--- UiPath.PowerShell/UiPath.PowerShell.psd1
+++ UiPath.PowerShell/UiPath.PowerShell.psd1
@@ -9,7 +9,7 @@
 @{
 
 # Script module or binary module file associated with this manifest.
-RootModule = 'lib\net461\UiPath.PowerShell.dll'
+RootModule = 'lib\net472\UiPath.PowerShell.dll'
 
 # Version number of this module.
 ModuleVersion = '$(UiPathFileVersion)'
@@ -57,7 +57,7 @@ CLRVersion = '4.0'
 #RequiredAssemblies = @()
 
 # Script files (.ps1) that are run in the caller's environment prior to importing this module.
-# ScriptsToProcess = @()
+ScriptsToProcess = @('.\tools\UiPath.BindingResolve.ps1')
 
 # Type files (.ps1xml) to be loaded when importing this module
 # TypesToProcess = @()
